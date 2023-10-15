3ed88680c35a4aa2ee016b9bbf2fcaeb697a86aa
xviiivin <88417506+xviiivin@users.noreply.github.com>
2023-10-15 17:15:47 +0700

    done

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+from datetime import datetime, timezone, timedelta
+
+app = Flask(__name__)
+tz = timezone(timedelta(hours=7))
+date = datetime.now(tz=tz)
+
+@app.route('/calculate_tax', methods=['POST'])
+def calculate_tax():
+    if request.is_json:
+        data = request.get_json()
+        currency = data.get('currency')
+        price = data.get('price')
+        payment_date = data.get('payment_date')
+        tax = "7%"
+        if ((currency is not None) and (price is not None) and (payment_date is not None)):
+            total_price = price * 1.07
+            response_data = {
+                "currency": currency,
+                "price": price,
+                "tax": tax,
+                "total_price": round(total_price, 2),
+                "payment_date": payment_date,
+                "date_after_tax": date.ctime()
+            }
+        else:
+            return jsonify({"message": "Invalid input data"}), 400
+        return jsonify({"message": "Tax calculated successfully", "data": response_data}), 200
+    else:
+        return jsonify({"message": "Invalid JSON format"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=False ,host='0.0.0.0')
